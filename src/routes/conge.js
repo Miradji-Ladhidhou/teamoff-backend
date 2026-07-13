8c07af0ec904f89c81b1bb2198e27aeb97c10365
Miradji-Ladhidhou <ladhidhoum@gmail.com>
2026-07-13 17:24:15 +0400

    Revert "feat(conges): endpoint GET /conges/:id/attestation"
    This reverts commit f3447ea56dc76a1d02948500f9087a73ce765e69.

--- src/routes/conge.js
+++ src/routes/conge.js
@@ -33,7 +33,6 @@ router.get('/:id/history', authorizeRole(['employe','manager','admin_entreprise'
     next(err);
   }
 });
-router.get('/:id/attestation', authorizeRole(['employe','manager','admin_entreprise','super_admin']), validateUUIDParam('id'), congeController.getAttestationData);
 router.post('/:id/validate', authorizeRole(['manager','admin_entreprise','super_admin']), validateUUIDParam('id'), advancedRateLimiter('conges'), congeController.validate);
 router.post('/:id/reject', authorizeRole(['manager','admin_entreprise','super_admin']), validateUUIDParam('id'), advancedRateLimiter('conges'), congeController.reject);
 
